71bf2aab587bd9b576b85ddd1e8f83406d816ee4
Osvaldo Lamas <osvaldo.lamas@synapse.com>
2018-04-28 19:50:23 -0300

    Adding modules to project

---
+++ social-media-aggregator-backend/src/main/java/com/olamas/socialmedia/socialmediaaggregatorbackend/SocialMediaAggregatorBackendApplication.java
@@ -0,0 +1,12 @@
+package com.olamas.socialmedia.socialmediaaggregatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAggregatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaAggregatorBackendApplication.class, args);
+	}
+}
---
+++ social-media-aggregator-backend/src/test/java/com/olamas/socialmedia/socialmediaaggregatorbackend/SocialMediaAggregatorBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.olamas.socialmedia.socialmediaaggregatorbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialMediaAggregatorBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
